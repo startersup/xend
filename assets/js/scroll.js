208118cf2c86fe8361f2965b241872af5b78589f
Saicharan <saicharan14996@gmail.com>
2021-05-06 19:42:59 +0530

    new updates on the pages
    new updates on the pages

---
+++ assets/js/scroll.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 100) {
+          $(".xd-navbar").addClass("xd-navbar-fixed")
+        }
+  
+        else{
+            $(".xd-navbar").removeClass("xd-navbar-fixed")
+        }
+    })
+  })
