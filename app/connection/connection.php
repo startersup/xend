5381b0a3a6b13a51cec48b2ac3e29febd4f5e970
Deepak12051996 <36324331+Deepak12051996@users.noreply.github.com>
2021-05-29 17:34:50 +0530

    db username issue

--- app/connection/connection.php
+++ app/connection/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn=mysqli_connect('localhost','u906351065_Q1ser2	','Smiley@Hateme@123','u906351065_pos');
+$conn=mysqli_connect('localhost','u906351065_Q1ser2','Smiley@Hateme@123','u906351065_pos');
 
 if( !$conn)
 {
