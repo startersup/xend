af68d2479285e3908e7c41efb5cda31a0dac999d
Deepak12051996 <36324331+Deepak12051996@users.noreply.github.com>
2021-05-30 11:49:41 +0530

    orders page

--- app/apis/v1/orders/index.php
+++ app/apis/v1/orders/index.php
@@ -4,7 +4,9 @@ session_start();
 
 $rootfolder = $_SERVER['DOCUMENT_ROOT'];
 include($rootfolder . "/connection/connection.php");
+$requestMethod = $_SERVER['REQUEST_METHOD'];
 
+if (strtoupper($requestMethod) == 'POST') {
 if ($_POST['data'] != "") {
     $_SESSION['USER_INFO']["product"] = array();
     $_SESSION['USER_INFO']["product"] = ($_POST['data']);
@@ -17,7 +19,7 @@ $address = $_SESSION['USER_INFO']['transaction']['address'];
 $payment = $_SESSION['USER_INFO']['transaction']['payment'];
 $address = $conn->real_escape_string($address);
 
-$insert_query_orders = " INSERT INTO `orders` ( `mobile`, `mail`, `name`, `address`, `pay_type`, `pay_status`) VALUES ( '$mobile', '$mailId', '$name', '$address', '$payment', 'pending') ";
+$insert_query_orders = " INSERT INTO `orders` ( `mobile`, `mail`, `name`, `address`, `pay_type`, `pay_status`,`status`) VALUES ( '$mobile', '$mailId', '$name', '$address', '$payment', 'pending','1') ";
 // $response["insert_query_orders"] = $insert_query_orders;
 $result =  mysqli_query($conn, $insert_query_orders);
 if ($result) {
@@ -26,18 +28,23 @@ if ($result) {
     $last_id =  $last_id + 1000;
     $last_id = "VSP" . $last_id;
     $update_query = " UPDATE `orders` SET `id` = '" . $last_id . "' WHERE `orders`.`sno` =  " . $update_id;
-   // $response["update_query"] = $update_query;
+    // $response["update_query"] = $update_query;
     $result =  mysqli_query($conn, $update_query);
 }
 
 
 $insert_query_items = " INSERT INTO `order_items` ( `order_id`, `item_name`) VALUES  ";
 $items_values = 'tempxxxx';
+$item_count = 0;
 foreach ($_SESSION['USER_INFO']['product'] as $itemData) {
     $item = (object) $itemData;
     $item_name = $conn->real_escape_string($item->product);
     $items_values = $items_values . " , ('" . $last_id . "', '" . $item_name . "')";
+    $item_count = $item_count + 1;
 }
+$update_query = " UPDATE `orders` SET `item_count` = " . $item_count . " WHERE `orders`.`sno` =  " . $update_id;
+$result =  mysqli_query($conn, $update_query);
+
 $items_values = str_replace("tempxxxx ,", "", $items_values);
 $insert_query_items = $insert_query_items . $items_values . " ; ";
 // $response["insert_query_items"] = $insert_query_items;
@@ -46,12 +53,27 @@ if ($result) {
     $response["status"] = true;
     $response["msg"] = "Order Placed Succesfully!";
     $response["order_id"] = $last_id;
-    $_SESSION['USER_INFO']=  array();
+    $_SESSION['USER_INFO'] =  array();
 } else {
     $delete_query = " DELETE FROM `orders` WHERE `sno` =  " . $update_id;
     // $response["delete_query"] = $delete_query;
-    $result =  mysqli_query($conn, $delete_query);    
+    $result =  mysqli_query($conn, $delete_query);
     $response["status"] = false;
     $response["msg"] = "Please try again";
 }
 echo json_encode($response);
+} else if (strtoupper($requestMethod) == 'GET') {
+
+    $sql_query = "SELECT `sno`, `id`, `mobile`, `mail`, `name`, `address`, `pay_type`, `pay_status`, `order_total`, `delivery_total`, `total`, `item_count`,( SELECT  dd.`optionText` FROM `dropDowns` as dd WHERE dd.`id` = 1 and dd.`optionValue` = od.`status` and `status` = 0 ) as Status, `tiktok` FROM `orders` as od WHERE od.status not in (4,5)";
+    $result =  mysqli_query($conn, $sql_query);   
+    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+
+        $temp[] = $row;
+    }
+    if ($temp == null) {
+        $temp = array();
+    }
+    $response["status"]=true;
+    $response["data"] = $temp;
+    
+}
---
+++ app/pos/admin/orders/index.php
@@ -0,0 +1,170 @@
+<?php
+
+$temp = $_SERVER['DOCUMENT_ROOT'];
+include($temp . "/connection/connection.php");
+$sql_query_order = "SELECT `sno`, `id`, `mobile`, `mail`, `name`, `address`, `pay_type`, `pay_status`, `order_total`, `delivery_total`, `total`, `item_count`,( SELECT  dd.`optionText` FROM `dropDowns` as dd WHERE dd.`id` = 1 and dd.`optionValue` = od.`status` and `status` = 0 ) as Status, `tiktok` FROM `orders` as od WHERE od.status not in (4,5)";
+$result_order =  mysqli_query($conn, $sql_query_order);   
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Xend PoS | Orders</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- <link rel="icon" href="../assets/images/xd-logo.png" type="image/svg" sizes="16x16"> -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="/assets/css/animation.css?3">
+  <link rel="stylesheet" href="/assets/css/dash.min.css?3">
+  <link rel="canonical" href="https://xendworks.com/" />
+</head>
+
+<body>
+  <nav class="xd-app-nav">
+    <span class="xd-title-left">XEND POS</span>
+    <div class="xd-user-wrapper">
+      <span class="xd-user-name xd-flex">
+        <div class="shortname"></div>
+        <div class="name" class="name" title="" data-id="Santhosh">Santhosh</div>
+      </span>
+      <div class="xd-action-dropdown">
+        <ul>
+          <li>Log out</li>
+          <li>View Sales</li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container">
+    <div class="xd-orders">
+      <div class="xd-flex xd-sticky">
+        <h3>Orders <span class="label label-success" id="orderCount"></span></h3>
+        <input class="form-control xd-searchbar" id="searchfilter" placeholder="Search for Orders">
+      </div>
+      <ul class="xd-orders-list" id="results">
+          <?php 
+          
+while ($row_order = mysqli_fetch_array($result_order, MYSQLI_ASSOC)) {
+    
+    ?>
+        <li>
+          <div class="xd-order-card">
+            <div class="xd-orderId xd-space">
+              <h5>Id</h5>
+              <p><?php echo($row_order["id"]); ?></p>
+            </div>
+            <div class="xd-orderName xd-space">
+              <h5>Customer Name</h5>
+              <p><?php echo($row_order["name"]); ?></p>
+            </div>
+            <div class="xd-orderNumber xd-space">
+              <h5>Contact Number</h5>
+              <p><?php echo($row_order["mobile"]); ?></p>
+            </div>
+            <div class="xd-orderItem xd-space">
+              <h5>Items Ordered</h5>
+              <p><?php echo($row_order["item_count"]); ?></p>
+            </div>
+            <div class="xd-orderPayType xd-space">
+              <h5>Pay Mode</h5>
+              <p><?php echo($row_order["pay_type"]); ?></p>
+              <input type="hidden" id="<?php echo("input_".$row_order["id"]); ?>" value = "<?php echo json_encode($row_order); ?>" >
+            </div>
+            <div class="xd-orderId xd-space">
+              <h5>Action</h5>
+              <a class="xd-process-order" data-toggle="modal" data-target="#myModal">Edit</a>
+            </div>
+          </div>
+        </li>
+
+<?php
+}
+
+?>
+
+      </ul>
+    </div>
+  </div>
+
+
+
+  <!-- Modal -->
+  <div class="modal bg xd-orderpopup" id="myModal" role="dialog">
+    <div class="modal-dialog modal-md">
+
+      <!-- Modal content-->
+
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">✕</button>
+          <h4 class="modal-title xd-poptitle">Order Id: VSP67382</h4>
+        </div>
+        <div class="modal-body xd-popup-body">
+          <h3>Order Items</h3>
+          <ul class="xd-order-items">
+            <li class="products">Maida Maavu 1kg <input class="xd-rate-box" placeholder="Enter Rate"><a> ❌ </a></li>
+            <li class="products">Kadalai Paarupu 200g <input class="xd-rate-box" placeholder="Enter Rate"><a> ❌ </a></li>
+            <li class="products">Meera shampoo - 3 Nos <input class="xd-rate-box" placeholder="Enter Rate"><a> ❌ </a></li>
+          </ul>
+          <div class="xd-grand-total">
+            <ul class="xd-order-items">
+              <li class="products">Grand Total<input class="xd-rate-box" disabled value="" placeholder=""></li>
+              </ul>
+          </div>
+        </div>
+        <div class="modal-footer pD20">
+          <a class="xd-cancel">Cancel Order</a>
+          <a class="xd-proceed">Proceed Order</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+<script>
+
+  var version = Math.floor(Math.random() * 100);
+  // document.write('<script src="https://urbanxperts.in/assets/js/common.js?dev=' +version + '"\><\/script>');
+  document.write('<script src="/assets/js/global.js?dev=' + version + '"\><\/script>');
+  document.write('<script src="/assets/js/serverCall.js?dev=' + version + '"\><\/script>');
+  document.write('<script src="/assets/js/mainSite.js?dev=' + version + '"\><\/script>');
+  document.write('<script src="/assets/js/login.js?dev=' + version + '"\><\/script>');
+     // document.write('<link rel="stylesheet" href="/assets/css/animation.css?dev=' + version + '">');
+       // document.write('<link rel="stylesheet" href="/assets/css/dash.min.css?dev=' + version + '">');
+
+</script>
+<script>
+  $("#searchfilter").keyup(function () {
+    var filter = $(this).val(),
+      count = 0;
+    $('#results .xd-order-card').each(function () {
+      if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+        $(this).hide();
+      } else {
+        $(this).show();
+        count++;
+      }
+    });
+
+  });
+</script>
+<script>
+  $('.name').each(function () {
+    var $name = $(this);
+    var str = $name.attr('data-id');
+    var matches = str.match(/\b(\w)/g);
+    var acronym = matches.join('');
+    $name.prev('.shortname').prepend('<div class="my-circle">' + acronym + '</div>');
+  });
+
+  var $orderCount = $('#results').find(".xd-order-card:visible").length;
+  $('#orderCount').html($orderCount);
+</script>
+
+
+</html>
