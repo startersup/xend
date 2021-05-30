5729366566ac06f2f2a33b2ecb73dfe5db9fb37b
Saicharan <saicharan14996@gmail.com>
2021-05-30 19:58:41 +0530

    fixes on css
    fixes on css

--- app/pos/admin/orders/index.php
+++ app/pos/admin/orders/index.php
@@ -23,8 +23,8 @@ $result_order =  mysqli_query($conn, $sql_query_order);
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <link rel="stylesheet" href="/assets/css/animation.css?3">
-    <link rel="stylesheet" href="/assets/css/dash.min.css?3">
+    <link rel="stylesheet" href="https://app.xendworks.com/pos/assets/css/animation.css?3">
+    <link rel="stylesheet" href="https://app.xendworks.com/pos/assets/css/dash.min.css?3">
     <link rel="canonical" href="https://xendworks.com/" />
 </head>
 
